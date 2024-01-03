4ed1df0fe165cfcd43ad3ad9658fd278076db88e
Mohamed Mansour <123316147+Mohamed-Mansou@users.noreply.github.com>
2024-01-04 01:58:01 +0300

    done

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve({
+      status: 200,
+      body: 'success',
+    });
+    reject(new Error('The fake API is not working currently'));
+  });
+}
